852095e94c760b67fe566d790dffa7640955899b
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-05-12 02:42:54 +0700

    feat: add method post

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 
 import { dataMotobikes } from "../data/mobobikes";
 
+let motobikes = dataMotobikes;
+
 const app = new Hono();
 
 // | `/motobikes`     | `GET`    | `Get all motobikes`      |
@@ -16,19 +18,35 @@ app.get("/", (c) => {
 });
 
 app.get("/motobikes", (c) => {
-  return c.json(dataMotobikes);
+  return c.json(motobikes);
 });
 
 app.get("/motobikes/:id", (c) => {
   const id = Number(c.req.param("id"));
-  const motobikes = dataMotobikes.find((motobikes) => motobikes.id == id);
+  const motobike = motobikes.find((motobikes) => motobikes.id == id);
 
-  if (!motobikes) {
+  if (!motobike) {
     c.status(404);
     return c.json({ message: "Motobikes Not Found" });
   }
 
-  return c.json(motobikes);
+  return c.json(motobike);
+});
+
+app.post("/motobikes", async (c) => {
+  const body = await c.req.json();
+
+  const nextId = motobikes[motobikes.length - 1].id + 1;
+
+  const newMotobike = {
+    id: nextId,
+    name: body.name,
+  };
+
+  motobikes  = [...motobikes, newMotobike];
+
+
+  return c.json({ motobike : newMotobike });
 });
 
 export default app;
