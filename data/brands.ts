f4254d28defd254487a8b85e79c9b1a02f8fac1b
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-06-08 12:46:16 +0700

    feat : add end point brands

--- data/brands.ts
+++ data/brands.ts
@@ -1,43 +1,49 @@
 import { type Brand } from "@prisma/client";
 
-export type DataBrand = Omit<Brand, "foundedAt" | "updatedAt">;
+export type DataBrand = Omit<Brand, "updatedAt">;
 
 export const dataBrands: DataBrand[] = [
   {
     id: 1,
     name: "Honda",
     founder: "Soichiro Honda, Takeo Fujisawa",
-    headqurter: "Minato, Tokyo, Jepang"
+    headqurter: "Minato, Tokyo, Jepang",
+    foundedAt: new Date("1948-09-24T15:02:51.219Z"),
   },
   {
     id: 2,
     name: "Yamaha",
     founder: "SGenichi Kawakami",
-    headqurter: "wata, Prefektur Shizuoka, Jepang"
+    headqurter: "wata, Prefektur Shizuoka, Jepang",
+    foundedAt: new Date("1955-07-01T15:02:51.219Z"),
   },
   {
     id: 3,
     name: "Kawasaki",
     founder: "Kawasaki Shōzō",
-    headqurter: "Minato, Tokyo, Jepang"
+    headqurter: "Minato, Tokyo, Jepang",
+    foundedAt: new Date("1896-10-15T15:02:51.219Z"),
   },
   {
     id: 4,
     name: "Suzuki",
     founder: "Michio Suzuki",
-    headqurter: "Hamamatsu, Prefektur Shizuoka, Jepang"
+    headqurter: "Hamamatsu, Prefektur Shizuoka, Jepang",
+    foundedAt: new Date("1909-10-01T15:02:51.219Z"),
   },
   {
     id: 5,
     name: "BMW",
     founder: "Susanne Klatten, Stefan Quandt",
-    headqurter: "München, Jerman"
+    headqurter: "München, Jerman",
+    foundedAt: new Date("1916-03-07T15:02:51.219Z"),
   },
   {
     id: 6,
     name: "Ducati",
-    founder: "Antonio Cavalieri Ducati, Adriano Cavalieri Ducati, Marcello Cavalieri Ducati, Bruno Cavalieri Ducati",
-    headqurter: "Borgo Panigale, Bologna, Italia"
+    founder:
+      "Antonio Cavalieri Ducati, Adriano Cavalieri Ducati, Marcello Cavalieri Ducati, Bruno Cavalieri Ducati",
+    headqurter: "Borgo Panigale, Bologna, Italia",
+    foundedAt: new Date("1926-07-04T15:02:51.219Z"),
   },
- 
 ];
--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ const app = new Hono();
 // | `/motorbikes`     | `DELETE` | `Delete all motorbikes`   |
 // | `/motorbikes/:id` | `DELETE` | `Delete motorbikes by id` |
 // | `/motorbikes/:id` | `PUT`    | `Update motorbikes by id` |
+// | `/brands`     | `GET`    | `Get all brands`      |
+// | `/brands/:id` | `GET`    | `Get brands by id`    |
+// | `/brands`     | `POST`   | `add new motorbike`       |
+// | `/brands`     | `DELETE` | `Delete all brands`   |
+// | `/brands/:id` | `DELETE` | `Delete brands by id` |
+// | `/brands/:id` | `PUT`    | `Update brands by id` |
 
 app.get("/", (c) => {
   return c.text("Database Motorbikes!");
@@ -89,7 +95,7 @@ app.put("/motorbikes/:id", async (c) => {
       name: body.name,
       cc: body.cc,
       type: body.type,
-      transmission: body.domain,
+      transmission: body.transmission,
       price: body.price,
       brandId: body.brandId,
     },
@@ -112,6 +118,71 @@ app.post("/brands/seed", async (c) => {
 app.get("/brands", async (c) => {
   const brands = await prisma.brand.findMany();
   return c.json(brands);
+});
+
+app.get("/brands/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+
+  const brand = await prisma.brand.findUnique({ where: { id } });
+
+  if (!brand) {
+    c.status(404);
+    return c.json({ message: "brand Not Found" });
+  }
+
+  return c.json(brand);
+});
+
+app.post("/brands", async (c) => {
+  const body = await c.req.json();
+
+  const brand = await prisma.brand.create({
+    data: {
+      name: body.name,
+      founder: body.founder,
+      foundedAt: body.foundedAt,
+      headqurter: body.headqurter,
+    },
+  });
+
+  return c.json({ brand });
+});
+
+app.delete("/brands", async (c) => {
+  const result = await prisma.brand.deleteMany();
+
+  return c.json({ messege: "All brands have been removed", result });
+});
+
+app.delete("/brands/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+
+  const deletedBrand = await prisma.brand.delete({ where: { id } });
+
+  return c.json({
+    messege: `Deleted brand with id ${id}`,
+    deletedBrand,
+  });
+});
+
+app.put("/brands/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+  const body = await c.req.json();
+
+  const updatedBrand = await prisma.brand.update({
+    where: { id },
+    data: {
+      name: body.name,
+      founder: body.founder,
+      foundedAt: body.foundedAt,
+      headqurter: body.headqurter,
+    },
+  });
+
+  return c.json({
+    mesage: `updated motorbike with id ${id}`,
+    brands: updatedBrand,
+  });
 });
 
 console.log({ messege: "motorbike is running" });
